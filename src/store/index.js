c485ce11bff92311eab63e93c5e869a8ec1acb19
Askarbek Karasasev <askar.karasaev@gmail.com>
2018-04-12 21:48:01 +0900

    Vuex Mutations

--- src/store/index.js
+++ src/store/index.js
@@ -3,24 +3,27 @@ import Vue from 'vue'
 
 Vue.use(Vuex)
 
-new Vuex.Store({
+export default new Vuex.Store({
     state: { // = data
         products: []
     },
 
     getters: { // = computed properties
-        productsCount () {
+        productsCount() {
             // ...
-        },
+        }
+    },
+    actions: {
+        fetchProducts() {
+            // make the call
+            // run setProducts mutation
+        }
+    },
 
-        actions: {
-            fetchProducts () {
-                // make the call
-            }
-        },
-
-        mutations: {
+    mutations: {
+        setProducts(state, products) {
             // update products
+            state.products = products
         }
     }
 })
