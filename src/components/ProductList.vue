e608e526e2873dfcdcda04b5a10bbdfe5468dad9
Askarbek Karasasev <askar.karasaev@gmail.com>
2018-04-15 16:57:44 +0900

    Add products to the cart

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -3,7 +3,10 @@
       <h1>Product List</h1>
       <img v-if="loading" src="https://i.imgur.com/JfPpwOA.gif">
       <ul v-else>
-          <li v-for="product in products">{{product.title}} - {{product.price}}</li>
+          <li v-for="product in products">
+              {{product.title}} - {{product.price}}
+              <button @click="addProductToCart(product)">Add to cart</button>
+              </li>
       </ul>
   </div>
 </template>
@@ -20,6 +23,13 @@ export default {
       return this.$store.getters.availableProducts;
     }
   },
+
+  methods: {
+      addProductToCart(product) {
+          this.$store.dispatch('addProductToCart', product)
+      }
+  },
+
   created() {
     this.loading = true;
     this.$store.dispatch("fetchProducts").then(() => (this.loading = false));
--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,9 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
     state: { // = data
-        products: []
+        products: [],
+        // {id, quantity}
+        cart: []
     },
 
     getters: { // = computed properties
@@ -24,6 +26,21 @@ export default new Vuex.Store({
                     resolve()
                 })
             })
+        },
+
+        addProductToCart(context, product) {
+            if (product.inventory > 0) {
+                // find cartItem
+                const cartItem = context.cart.find(item => item.id === product.id)
+                if (!cartItem) {
+                    // pushProductToCart
+                    context.commit('pushProductToCart', product.id)
+                } else {
+                    //incerementItemQuantity
+                    context.commit('incrementItemQuantity', cartItem)
+                }
+                context.commit('decrementProductInventory', product)
+            }
         }
     },
 
@@ -31,6 +48,21 @@ export default new Vuex.Store({
         setProducts(state, products) {
             // update products
             state.products = products
+        },
+
+        pushProductToCart(state, productId) {
+            state.cart.push({
+                id: productId,
+                quantity: 1
+            })
+        },
+
+        incrementItemQuantity(state, cartItem) {
+            cartItem.quantity++
+        },
+
+        decrementProductInventory(state, product) {
+            product.inventory--
         }
     }
 })
